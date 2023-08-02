46908334ceec86c99afa6201730392f46ea75def
Plüss Roland <roland@rptd.ch>
2023-08-02 13:36:17 +0200

    added indominusrex example character

--- updateInitialData.sh
+++ updateInitialData.sh
@@ -6,3 +6,4 @@ export DIR_CONFIG=`realpath distribute/config/characters`
 xsltproc --output "${DIR_INITIAL}/Biomech.demccp" copyInitialCharacter.xsl "${DIR_CONFIG}/Biomech.demccp"
 xsltproc --output "${DIR_INITIAL}/Dragonroo.demccp" copyInitialCharacter.xsl "${DIR_CONFIG}/Dragonroo.demccp"
 xsltproc --output "${DIR_INITIAL}/First Person Hands.demccp" copyInitialCharacter.xsl "${DIR_CONFIG}/First Person Hands.demccp"
+xsltproc --output "${DIR_INITIAL}/Indominus Rex.demccp" copyInitialCharacter.xsl "${DIR_CONFIG}/Indominus Rex.demccp"
