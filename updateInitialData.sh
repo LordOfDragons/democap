c70813ac74d826523f9012a7dda4af5c160b6bea
Plüss Roland <roland@rptd.ch>
2024-07-30 12:20:56 +0200

    initial data updates

--- updateInitialData.sh
+++ updateInitialData.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export DIR_INITIAL=`realpath data/content/initial/config/characters`
-export DIR_CONFIG=`realpath distribute/config/characters`
+export DIR_CONFIG=`realpath cache/testrun/config/characters`
 
 xsltproc --output "${DIR_INITIAL}/Biomech.demccp" copyInitialCharacter.xsl "${DIR_CONFIG}/Biomech.demccp"
 xsltproc --output "${DIR_INITIAL}/Dragonroo.demccp" copyInitialCharacter.xsl "${DIR_CONFIG}/Dragonroo.demccp"
